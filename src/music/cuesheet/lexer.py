674b9e95d8079aebd3ed237ac3dc513a008189e6
Don McCaughey <don@donm.cc>
2025-06-29 22:09:57 -0700

    Add the `Lexer.commands` class attribute.
    Move the `command_type_map` to `Lexer.commands`.

--- src/music/cuesheet/lexer.py
+++ src/music/cuesheet/lexer.py
@@ -15,20 +15,16 @@ from .commands import (
 )
 
 
-command_type_map: dict[str, CommandType] = {
-    command_type.__name__.upper(): command_type
-    for command_type in [
-        File,
-        Index,
-        Performer,
-        Rem,
-        Title,
-        Track,
-    ]
-}
-
-
 class Lexer:
+    commands: dict[str, CommandType] = {
+        'FILE': File,
+        'INDEX': Index,
+        'PERFORMER': Performer,
+        'REM': Rem,
+        'TITLE': Title,
+        'TRACK': Track,
+    }
+
     def scan(self, source: TextIO) -> Generator[Line]:
         for i, line in enumerate(source):
             yield self.scan_line(i + 1, line.strip('\n'))
@@ -40,8 +36,8 @@ class Lexer:
             return Blank(line_number, line)
 
         command_name = tokens[0]
-        if command_name in command_type_map:
-            command_type = command_type_map[command_name]
+        if command_name in self.commands:
+            command_type = self.commands[command_name]
             command = command_type.parse(line_number, line)
             return command if command else Error(line_number, line)
 
