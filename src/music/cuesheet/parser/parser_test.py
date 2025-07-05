c751a06c781915fbc9c96ffbdbdef7b8a65b6756
Don McCaughey <don@donm.cc>
2025-07-05 12:40:46 -0700

    Add track info to parser test.

--- src/music/cuesheet/parser/parser_test.py
+++ src/music/cuesheet/parser/parser_test.py
@@ -18,7 +18,7 @@ def test_parser_for_normal_file():
         #
         Token(2, TokenType.NAME, 'TITLE'),
         Token(2, TokenType.WS, ' '),
-        Token(2, TokenType.QSTR, 'Gimme Shelter'),
+        Token(2, TokenType.QSTR, 'Let It Bleed'),
         Token(2, TokenType.EOL, '\n'),
         #
         Token(3, TokenType.WS, ' '),
@@ -31,6 +31,7 @@ def test_parser_for_normal_file():
         Token(4, TokenType.NAME, 'WAVE'),
         Token(4, TokenType.EOL, '\n'),
         #
+        Token(5, TokenType.WS, '  '),
         Token(5, TokenType.NAME, 'TRACK'),
         Token(5, TokenType.WS, ' '),
         Token(5, TokenType.INT, 1),
@@ -38,12 +39,37 @@ def test_parser_for_normal_file():
         Token(5, TokenType.NAME, 'AUDIO'),
         Token(5, TokenType.EOL, '\n'),
         #
+        Token(6, TokenType.WS, '    '),
+        Token(6, TokenType.NAME, 'TITLE'),
+        Token(6, TokenType.WS, ' '),
+        Token(6, TokenType.QSTR, 'Gimme Shelter'),
+        Token(6, TokenType.EOL, '\n'),
+        #
+        Token(7, TokenType.WS, '    '),
+        Token(7, TokenType.NAME, 'PERFORMER'),
+        Token(7, TokenType.WS, ' '),
+        Token(7, TokenType.QSTR, 'The Rolling Stones'),
+        Token(7, TokenType.EOL, '\n'),
+        #
+        Token(9, TokenType.WS, '  '),
         Token(9, TokenType.NAME, 'TRACK'),
         Token(9, TokenType.WS, ' '),
         Token(9, TokenType.INT, 2),
         Token(9, TokenType.WS, ' '),
         Token(9, TokenType.NAME, 'AUDIO'),
         Token(9, TokenType.EOL, '\n'),
+        #
+        Token(10, TokenType.WS, '    '),
+        Token(10, TokenType.NAME, 'TITLE'),
+        Token(10, TokenType.WS, ' '),
+        Token(10, TokenType.QSTR, 'Love in Vain'),
+        Token(10, TokenType.EOL, '\n'),
+        #
+        Token(11, TokenType.WS, '    '),
+        Token(11, TokenType.NAME, 'PERFORMER'),
+        Token(11, TokenType.WS, ' '),
+        Token(11, TokenType.QSTR, 'The Rolling Stones'),
+        Token(11, TokenType.EOL, '\n'),
     ]
     parser = Parser(iter(tokens))
 
@@ -58,7 +84,7 @@ def test_parser_for_normal_file():
             ),
             Title(
                 tokens=[
-                    Token(2, TokenType.QSTR, 'Gimme Shelter'),
+                    Token(2, TokenType.QSTR, 'Let It Bleed'),
                 ],
                 children=[],
             ),
@@ -73,14 +99,44 @@ def test_parser_for_normal_file():
                             Token(5, TokenType.INT, 1),
                             Token(5, TokenType.NAME, 'AUDIO'),
                         ],
-                        children=[],
+                        children=[
+                            Title(
+                                tokens=[
+                                    Token(6, TokenType.QSTR, 'Gimme Shelter'),
+                                ],
+                                children=[],
+                            ),
+                            Performer(
+                                tokens=[
+                                    Token(
+                                        7, TokenType.QSTR, 'The Rolling Stones'
+                                    ),
+                                ],
+                                children=[],
+                            ),
+                        ],
                     ),
                     Track(
                         tokens=[
                             Token(9, TokenType.INT, 2),
                             Token(9, TokenType.NAME, 'AUDIO'),
                         ],
-                        children=[],
+                        children=[
+                            Title(
+                                tokens=[
+                                    Token(10, TokenType.QSTR, 'Love in Vain'),
+                                ],
+                                children=[],
+                            ),
+                            Performer(
+                                tokens=[
+                                    Token(
+                                        11, TokenType.QSTR, 'The Rolling Stones'
+                                    ),
+                                ],
+                                children=[],
+                            ),
+                        ],
                     ),
                 ],
             ),
