7adae612385e2ba427b9e540fb12e3e63699d62d
Don McCaughey <don@donm.cc>
2025-07-05 12:31:12 -0700

    Unparameterize the parser test.

--- src/music/cuesheet/parser/parser_test.py
+++ src/music/cuesheet/parser/parser_test.py
@@ -1,5 +1,3 @@
-import pytest
-
 from music.cuesheet.lexer.token import Token
 from music.cuesheet.lexer.token_type import TokenType
 
@@ -11,83 +9,80 @@ from .title import Title
 from .track import Track
 
 
-@pytest.mark.parametrize(
-    'tokens, expected',
-    [
-        (
-            [
-                Token(1, TokenType.NAME, 'PERFORMER'),
-                Token(1, TokenType.WS, ' '),
-                Token(1, TokenType.QSTR, 'The Rolling Stones'),
-                Token(1, TokenType.EOL, '\n'),
-                Token(2, TokenType.NAME, 'TITLE'),
-                Token(2, TokenType.WS, ' '),
-                Token(2, TokenType.QSTR, 'Gimme Shelter'),
-                Token(2, TokenType.EOL, '\n'),
-                Token(3, TokenType.NAME, 'FILE'),
-                Token(3, TokenType.WS, ' '),
-                Token(3, TokenType.QSTR, 'album.wav'),
-                Token(3, TokenType.WS, ' '),
-                Token(3, TokenType.NAME, 'WAVE'),
-                Token(3, TokenType.EOL, '\n'),
-                Token(4, TokenType.WS, ' '),
-                Token(4, TokenType.EOL, '\n'),
-                Token(5, TokenType.NAME, 'TRACK'),
-                Token(5, TokenType.WS, ' '),
-                Token(5, TokenType.INT, 1),
-                Token(5, TokenType.WS, ' '),
-                Token(5, TokenType.NAME, 'AUDIO'),
-                Token(5, TokenType.EOL, '\n'),
-                Token(9, TokenType.NAME, 'TRACK'),
-                Token(9, TokenType.WS, ' '),
-                Token(9, TokenType.INT, 2),
-                Token(9, TokenType.WS, ' '),
-                Token(9, TokenType.NAME, 'AUDIO'),
-                Token(9, TokenType.EOL, '\n'),
-            ],
-            Root(
-                tokens=[],
+def test_parser_for_normal_file():
+    tokens = [
+        Token(1, TokenType.NAME, 'PERFORMER'),
+        Token(1, TokenType.WS, ' '),
+        Token(1, TokenType.QSTR, 'The Rolling Stones'),
+        Token(1, TokenType.EOL, '\n'),
+        #
+        Token(2, TokenType.NAME, 'TITLE'),
+        Token(2, TokenType.WS, ' '),
+        Token(2, TokenType.QSTR, 'Gimme Shelter'),
+        Token(2, TokenType.EOL, '\n'),
+        #
+        Token(3, TokenType.WS, ' '),
+        Token(3, TokenType.EOL, '\n'),
+        #
+        Token(4, TokenType.NAME, 'FILE'),
+        Token(4, TokenType.WS, ' '),
+        Token(4, TokenType.QSTR, 'album.wav'),
+        Token(4, TokenType.WS, ' '),
+        Token(4, TokenType.NAME, 'WAVE'),
+        Token(4, TokenType.EOL, '\n'),
+        #
+        Token(5, TokenType.NAME, 'TRACK'),
+        Token(5, TokenType.WS, ' '),
+        Token(5, TokenType.INT, 1),
+        Token(5, TokenType.WS, ' '),
+        Token(5, TokenType.NAME, 'AUDIO'),
+        Token(5, TokenType.EOL, '\n'),
+        #
+        Token(9, TokenType.NAME, 'TRACK'),
+        Token(9, TokenType.WS, ' '),
+        Token(9, TokenType.INT, 2),
+        Token(9, TokenType.WS, ' '),
+        Token(9, TokenType.NAME, 'AUDIO'),
+        Token(9, TokenType.EOL, '\n'),
+    ]
+    parser = Parser(iter(tokens))
+
+    assert parser.parse() == Root(
+        tokens=[],
+        children=[
+            Performer(
+                tokens=[
+                    Token(1, TokenType.QSTR, 'The Rolling Stones'),
+                ],
+                children=[],
+            ),
+            Title(
+                tokens=[
+                    Token(2, TokenType.QSTR, 'Gimme Shelter'),
+                ],
+                children=[],
+            ),
+            File(
+                tokens=[
+                    Token(4, TokenType.QSTR, 'album.wav'),
+                    Token(4, TokenType.NAME, 'WAVE'),
+                ],
                 children=[
-                    Performer(
+                    Track(
                         tokens=[
-                            Token(1, TokenType.QSTR, 'The Rolling Stones'),
+                            Token(5, TokenType.INT, 1),
+                            Token(5, TokenType.NAME, 'AUDIO'),
                         ],
                         children=[],
                     ),
-                    Title(
+                    Track(
                         tokens=[
-                            Token(2, TokenType.QSTR, 'Gimme Shelter'),
+                            Token(9, TokenType.INT, 2),
+                            Token(9, TokenType.NAME, 'AUDIO'),
                         ],
                         children=[],
-                    ),
-                    File(
-                        tokens=[
-                            Token(3, TokenType.QSTR, 'album.wav'),
-                            Token(3, TokenType.NAME, 'WAVE'),
-                        ],
-                        children=[
-                            Track(
-                                tokens=[
-                                    Token(5, TokenType.INT, 1),
-                                    Token(5, TokenType.NAME, 'AUDIO'),
-                                ],
-                                children=[],
-                            ),
-                            Track(
-                                tokens=[
-                                    Token(9, TokenType.INT, 2),
-                                    Token(9, TokenType.NAME, 'AUDIO'),
-                                ],
-                                children=[],
-                            ),
-                        ],
                     ),
                 ],
             ),
-        ),
-    ],
-)
-def test_parser(tokens, expected):
-    parser = Parser(iter(tokens))
-
-    assert parser.parse() == expected
+        ],
+    )
