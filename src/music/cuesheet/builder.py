6261a9bea708358bc46f3a714efb8cf40d1926db
Don McCaughey <don@donm.cc>
2025-07-11 21:01:37 -0700

    Rename local variable `cuesheet` to `cue_sheet`.

--- src/music/cuesheet/builder.py
+++ src/music/cuesheet/builder.py
@@ -22,34 +22,34 @@ class Builder:
         self.root = root
 
     def build_cue_sheet(self) -> CueSheet2:
-        cuesheet = CueSheet2()
+        cue_sheet = CueSheet2()
         for child in self.root.children:
             match child:
                 case ASIN() as asin_node:
-                    cuesheet.asin = asin_node.value
+                    cue_sheet.asin = asin_node.value
                 case Comment() as comment_node:
-                    cuesheet.comment = comment_node.value
+                    cue_sheet.comment = comment_node.value
                 case DiscID() as disc_id:
-                    cuesheet.disc_id = disc_id.value
+                    cue_sheet.disc_id = disc_id.value
                 case Error() as error_node:
-                    cuesheet.errors.append(
+                    cue_sheet.errors.append(
                         Error2(error_node.line_num, error_node.value)
                     )
                 case File() as file_node:
-                    cuesheet.file = self._build_file(file_node)
+                    cue_sheet.file = self._build_file(file_node)
                 case Genre() as genre_node:
-                    cuesheet.genre = genre_node.value
+                    cue_sheet.genre = genre_node.value
                 case Performer() as performer_node:
-                    cuesheet.performer = performer_node.value
+                    cue_sheet.performer = performer_node.value
                 case Rem() as rem_node:
-                    cuesheet.remarks.append(rem_node.value)
+                    cue_sheet.remarks.append(rem_node.value)
                 case Title() as title_node:
-                    cuesheet.title = title_node.value
+                    cue_sheet.title = title_node.value
                 case Year() as year_node:
-                    cuesheet.year = year_node.value
+                    cue_sheet.year = year_node.value
                 case _:
                     raise RuntimeError(f'Unexpected node {child}')
-        return cuesheet
+        return cue_sheet
 
     def _build_file(self, file_node: File) -> File2:
         file = File2(file_node.filename, file_node.type)
